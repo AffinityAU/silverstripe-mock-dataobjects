0a26d30a958d915efeca39387bc6a703c7123152
Jiggs Jaiswal <jignesh@affinity.ad>
2017-08-07 12:49:40 +1000

    Ensure compatibility of the module for silverstripe frameworks post 3.5.4

--- _config.php
+++ _config.php
@@ -9,7 +9,15 @@ define('MOCK_DATAOBJECTS_DIR',basename(dirname(__FILE__)));
 Object::add_extension('DBField', 'MockDBField');
 
 foreach(SS_ClassLoader::instance()->getManifest()->getDescendantsOf("DBField") as $class) {
-	$mockClass = "Mock{$class}Field";
+
+    // Ensure that if the framework is 3.6.0 +, Float and Int fields are mapped appropriately to their mock<<db>>field classes.
+    if($class == "DBFloat")
+        $mockClass = 'MockFloatField';
+    else if($class == "DBInt")
+        $mockClass = 'MockIntField';
+    else
+        $mockClass = "Mock{$class}Field";
+
 	if(class_exists($mockClass)) {
 		Object::add_extension($class, $mockClass);
 	}
